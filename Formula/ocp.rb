eefe7d4fe1c6e3644fa155a55ce33fec90086b0d
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-03-23 18:41:24 +0100

    Refactor OCP version handling in ocp.rb

--- Formula/ocp.rb
+++ Formula/ocp.rb
@@ -1,28 +1,30 @@
 class Ocp < Formula
   desc "OpenCode configuration profiles CLI"
   homepage "https://github.com/alvarosanchez/ocp"
-  version "1.0.0-RC1"
   license "Apache-2.0"
 
   depends_on "bat"
 
+  OCP_VERSION = "1.0.0-RC1"
+
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/alvarosanchez/ocp/releases/download/v#{version}/ocp-#{version}-macos-aarch64"
+    url "https://github.com/alvarosanchez/ocp/releases/download/v#{OCP_VERSION}/ocp-#{OCP_VERSION}-macos-aarch64"
     sha256 "84c20b518af9ef8c960c2cbbe5ccc174c879d3b7858e79f7c2f56b3c1967f26e"
   elsif OS.mac?
-    url "https://github.com/alvarosanchez/ocp/releases/download/v#{version}/ocp-#{version}-macos-x86_64"
+    url "https://github.com/alvarosanchez/ocp/releases/download/v#{OCP_VERSION}/ocp-#{OCP_VERSION}-macos-x86_64"
     sha256 "d17269830032c07c51bcf37b0197fd8420ffd3b920786aa6474cb57f67a4d397"
   else
-    url "https://github.com/alvarosanchez/ocp/releases/download/v#{version}/ocp-#{version}-linux-x86_64"
+    url "https://github.com/alvarosanchez/ocp/releases/download/v#{OCP_VERSION}/ocp-#{OCP_VERSION}-linux-x86_64"
     sha256 "4d4592aab925bc9c785c5aabb1c3e1abc5b628380d25e21c93f41643ce339a86"
   end
 
   def install
-    binary_name = if OS.mac?
-      Hardware::CPU.arm? ? "ocp-#{version}-macos-aarch64" : "ocp-#{version}-macos-x86_64"
-    else
-      "ocp-#{version}-linux-x86_64"
-    end
+    binary_name =
+      if OS.mac?
+        Hardware::CPU.arm? ? "ocp-#{OCP_VERSION}-macos-aarch64" : "ocp-#{OCP_VERSION}-macos-x86_64"
+      else
+        "ocp-#{OCP_VERSION}-linux-x86_64"
+      end
 
     bin.install binary_name => "ocp"
   end
